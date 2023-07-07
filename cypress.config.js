153a175872521e7ea48eab68c73b24f87046b809
Bruno Pinheiro <brunopnhro@gmail.com>
2023-07-07 16:45:57 -0400

    resolvendo problema eslint

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
-    baseUrl:'https://notes-serverless-app.com',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl:'https://notes-serverless-app.com'
   },
 })
